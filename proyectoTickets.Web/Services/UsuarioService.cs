5e34156bb0cfb2556e9d25b8ccce77e75a2ae2b2
Alejandra Hernandez <alejandra.hernandez3@catolica.edu.sv>
2025-06-01 09:58:59 -0600

    mejorando create usuario vista

--- proyectoTickets.Web/Services/UsuarioService.cs
+++ proyectoTickets.Web/Services/UsuarioService.cs
@@ -1,4 +1,6 @@
 ﻿using proyectoTickets.Web.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace proyectoTickets.Web.Services
 {
@@ -26,9 +28,28 @@ namespace proyectoTickets.Web.Services
             return await _httpClient.GetFromJsonAsync<Usuario>($"api/Usuarios/{id}");
         }
 
+        public  string EncryptToMD5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to a hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+
         public async Task<bool> CreateUsuarioAsync(Usuario usuario)
         {
+            usuario.PasswordHash = EncryptToMD5(usuario.PasswordHash); 
             var response = await _httpClient.PostAsJsonAsync("api/Usuarios", usuario);
+            usuario.FechaCreacion = DateTime.Now;    
             return response.IsSuccessStatusCode;
         }
 
